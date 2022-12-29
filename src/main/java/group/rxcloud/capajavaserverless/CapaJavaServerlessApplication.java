c237d0c2df4ef35d31b0d73d3b102e6730945bb4
kevinten <596823919@qq.com>
2022-12-29 22:39:16 +0800

    init

---
+++ src/main/java/group/rxcloud/capajavaserverless/CapaJavaServerlessApplication.java
@@ -0,0 +1,13 @@
+package group.rxcloud.capajavaserverless;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapaJavaServerlessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapaJavaServerlessApplication.class, args);
+	}
+
+}
